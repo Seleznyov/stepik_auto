a071bbfc7a4d3ab20b8ed1ca9b9b515f50ebd87b
Seleznyov <58526725+Seleznyov@users.noreply.github.com>
2021-02-13 20:13:23 +0300

    section3_task3_6

---
+++ 3/test_items.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.common.exceptions import NoSuchElementException
+
+def test_is_element_present(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(3)
+    try:
+        browser.find_elements_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
+        result = True
+    except NoSuchElementException:
+        result = False
+    assert result == True, "Element not found"
+
+
+
