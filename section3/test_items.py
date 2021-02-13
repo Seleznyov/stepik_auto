99409c33b2ef063c2bae72b6943986674a7da923
Seleznyov <58526725+Seleznyov@users.noreply.github.com>
2021-02-13 20:28:26 +0300

    (Задание: запуск автотестов для разных языков интерфейса)

--- section3/test_items.py
+++ section3/test_items.py
@@ -4,14 +4,13 @@ from selenium.common.exceptions import NoSuchElementException
 def test_is_element_present(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     try:
         browser.find_elements_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
         result = True
     except NoSuchElementException:
         result = False
     assert result == True, "Element not found"
-    # Папка для проверки(Задание: запуск автотестов для разных языков интерфейса)
 
 
 
